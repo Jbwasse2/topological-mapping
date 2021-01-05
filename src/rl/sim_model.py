45a59ef4deb43c2e3830cc42a6697605f2f8a3c5
Justin W <jbwasse2@illinois.edu>
2021-01-05 17:32:45 -0600

    Starting to put ddppo into my stuff

--- src/rl/sim_model.py
+++ src/rl/sim_model.py
@@ -1,18 +1,35 @@
-import pudb
-import habitat
-from habitat_baselines.utils.env_utils import construct_envs
-from habitat_baselines.common.environments import get_env_class
+import gzip
 import os
+import random
 import time
+from copy import deepcopy
 from typing import ClassVar, Dict, List
 
+import habitat
+import networkx as nx
+import numpy as np
+import pudb
 import torch
-
 from habitat import Config, logger
+from habitat_baselines.common.environments import get_env_class
 from habitat_baselines.common.tensorboard_utils import TensorboardWriter
-from habitat_baselines.utils.common import poll_checkpoint_folder
 from habitat_baselines.config.default import get_config
 from habitat_baselines.rl.ppo.ppo_trainer import PPOTrainer
+from habitat_baselines.utils.common import poll_checkpoint_folder
+from habitat_baselines.utils.env_utils import construct_envs
+
+
+def get_dict(fname):
+    f = gzip.open(
+        "../../data/datasets/pointnav/gibson/v2/train_large/content/"
+        + fname
+        + ".json.gz"
+    )
+    content = f.read()
+    content = content.decode()
+    content = content.replace("null", "None")
+    content = eval(content)
+    return content["episodes"]
 
 
 def get_checkpoint(config):
@@ -66,16 +83,81 @@ def example_forward(model, hidden_state, scene, device):
     model.act(ob, hidden_state, prev_action, not_done_masks, deterministic=False)
 
 
+def try_to_reach(G, start_node, end_node, model, hidden_state, scene, device):
+    # Perform high level planning over graph
+    try:
+        path = nx.dijkstra_path(G, start_node, end_node)
+    except nx.exception.NetworkXNoPath as e:
+        return 2
+    pu.db
+    # Move robot to starting position/heading
+    current_node = path[0]
+    local_goal = path[1]
+    for i in range(len(path) - 1):
+        success = try_to_reach_local(
+            current_node, local_goal, model, hidden_state, scene, device
+        )
+        if not success:
+            return 1
+        else:
+            current_node = local_goal
+            local_goal = path[i + 2]
+    return 0
+
+
+def try_to_reach_local(start_node, local_goal_node, model, hidden_state, scene, device):
+    MAX_NUMBER_OF_STEPS = 50
+    prev_action = torch.zeros(1, 1).to(device)
+    not_done_masks = torch.zeros(1, 1).to(device)
+    ob = scene.reset()
+    for i in range(MAX_NUMBER_OF_STEPS):
+        pass
+
+
+def visualize_observation(observation):
+    pass
+
+
+def run_experiment(G, model, hidden_state, scene, device, experiments=100):
+    # Choose 2 random nodes in graph
+    return_codes = [0 for i in range(3)]
+    for _ in range(experiments):
+        node1, node2 = get_two_nodes(G)
+        # Hard code for now...
+        node1 = (13, 10)
+        node2 = (31, 60)
+        results = try_to_reach(
+            G, node1, node2, model, deepcopy(hidden_state), scene, device
+        )
+        return_codes[results] += 1
+    return return_codes
+
+
+# Currently just return 2 random nodes, in the future may do something smarter.
+def get_two_nodes(G):
+    return random.sample(list(G.nodes()), 2)
+
+
 def main():
+    random.seed(0)
     config = get_config("configs/baselines/ddppo_pointnav.yaml", [])
     device = (
         torch.device("cuda", config.TORCH_GPU_ID)
         if torch.cuda.is_available()
         else torch.device("cpu")
     )
-    scene = create_sim("Airport")
-    model, hidden_state = get_ddppo_model(config, device)
-    example_forward(model, hidden_state, scene, device)
+    scene = create_sim("Goodwine")
+    #  model, hidden_state = get_ddppo_model(config, device)
+    #  example_forward(model, hidden_state, scene, device)
+    G = nx.read_gpickle("./data/map/map_Goodwine.gpickle")
+    d = get_dict("Goodwine")
+    #    traj_ind = np.load("./traj_ind.npy", allow_pickle=True)
+    #    traj_ind_eval = np.load("./traj_ind_eval.npy", allow_pickle=True)
+    #    traj_new = np.load("./traj_new.npy", allow_pickle=True)
+    #    traj_new_eval = np.load("./traj_new_eval.npy", allow_pickle=True)
+    #    eval_trajs = np.load("./eval_trajs.npy", allow_pickle=True)
+    run_experiment(G, None, None, scene, device)
+    pu.db
 
 
 if __name__ == "__main__":
