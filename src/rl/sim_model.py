3651756ac98d126889cb2663aba01c9d1ecab67d
Justin W <jbwasse2@illinois.edu>
2021-01-07 16:15:18 -0600

    fixed position

--- src/rl/sim_model.py
+++ src/rl/sim_model.py
@@ -13,12 +13,20 @@ import numpy as np
 import pudb
 import torch
 from habitat import Config, logger
+from habitat.tasks.utils import cartesian_to_polar
+from habitat.utils.geometry_utils import (
+    agent_state_target2ref,
+    angle_between_quaternions,
+    quaternion_rotate_vector,
+)
 from habitat_baselines.common.environments import get_env_class
 from habitat_baselines.common.tensorboard_utils import TensorboardWriter
 from habitat_baselines.config.default import get_config
 from habitat_baselines.rl.ppo.ppo_trainer import PPOTrainer
 from habitat_baselines.utils.common import poll_checkpoint_folder
 from habitat_baselines.utils.env_utils import construct_envs
+from quaternion import as_euler_angles, quaternion
+from tqdm import tqdm
 
 
 def get_dict(fname):
@@ -56,6 +64,7 @@ def get_ddppo_model(config, device):
     trainer._setup_actor_critic_agent(ppo_cfg)
     trainer.agent.load_state_dict(ckpt_dict["state_dict"])
     actor_critic = trainer.agent.actor_critic
+    actor_critic.eval()
     test_recurrent_hidden_states = torch.zeros(
         actor_critic.net.num_recurrent_layers,
         config.NUM_PROCESSES,
@@ -77,7 +86,6 @@ def create_sim(scene):
 
 def example_forward(model, hidden_state, scene, device):
     ob = scene.reset()
-    pu.db
     prev_action = torch.zeros(1, 1).to(device)
     not_done_masks = torch.zeros(1, 1).to(device)
     ob["pointgoal_with_gps_compass"] = torch.rand(1, 2).to(device)
@@ -93,13 +101,21 @@ def get_node_pose(node, d):
     return position, rotation
 
 
-def try_to_reach(G, start_node, end_node, d, model, hidden_state, sim, device):
+def try_to_reach(
+    G, start_node, end_node, d, model, hidden_state, sim, device, visualize=True
+):
     ob = sim.reset()
     # Perform high level planning over graph
+    if visualize:
+        video_name = "local.mkv"
+        video = cv2.VideoWriter(video_name, 0, 3, (256, 256 * 3))
+    else:
+        video = None
     try:
         path = nx.dijkstra_path(G, start_node, end_node)
     except nx.exception.NetworkXNoPath as e:
         return 2
+    print("NEW PATH")
     current_node = path[0]
     local_goal = path[1]
     # Move robot to starting position/heading
@@ -109,15 +125,18 @@ def try_to_reach(G, start_node, end_node, d, model, hidden_state, sim, device):
     agent_state.rotation = rot
     sim.agents[0].set_state(agent_state)
     # Start experiments!
-    for i in range(len(path) - 1):
+    for current_node, local_goal in zip(path, path[1:]):
         success = try_to_reach_local(
-            current_node, local_goal, d, model, hidden_state, sim, device
+            current_node, local_goal, d, model, hidden_state, sim, device, video
         )
-        if not success:
+        if success != 1:
+            if visualize:
+                cv2.destroyAllWindows()
+                video.release()
             return 1
-        else:
-            current_node = local_goal
-            local_goal = path[i + 2]
+    if visualize:
+        cv2.destroyAllWindows()
+        video.release()
     return 0
 
 
@@ -135,59 +154,80 @@ def get_node_image(node, scene_name):
     return plt.imread(image_location)
 
 
-# Returns 1 on success, and 0 on failure
+# Returns 1 on success, and 0 or -1 on failure
 def try_to_reach_local(
-    start_node, local_goal_node, d, model, hidden_state, sim, device, visualize=True
+    start_node, local_goal_node, d, model, hidden_state, sim, device, video
 ):
-    pu.db
     MAX_NUMBER_OF_STEPS = 50
     prev_action = torch.zeros(1, 1).to(device)
     not_done_masks = torch.zeros(1, 1).to(device)
     ob = sim.get_observations_at(sim.get_agent_state())
     actions = []
-    if visualize:
+    if video is not None:
         scene_name = os.path.splitext(os.path.basename(sim.config.sim_cfg.scene.id))[0]
 
         start_image = cv2.resize(get_node_image(start_node, scene_name), (256, 256))
         goal_image = cv2.resize(get_node_image(local_goal_node, scene_name), (256, 256))
-        # visualize_observation(ob, start_image, goal_image)
-        pu.db
-        video_name = "local.mkv"
-        video = cv2.VideoWriter(video_name, 0, 3, (256, 256 * 3))
         image = np.vstack([ob["rgb"], start_image, goal_image])
+        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
         video.write(image)
 
     for i in range(MAX_NUMBER_OF_STEPS):
         displacement = torch.from_numpy(
             get_displacement_local_goal(sim, local_goal_node, d)
         ).type(torch.float32)
+        print(displacement)
         ob["pointgoal_with_gps_compass"] = displacement.unsqueeze(0).to(device)
         ob["depth"] = torch.from_numpy(ob["depth"]).unsqueeze(0).to(device)
         _, action, _, hidden_state = model.act(
             ob, hidden_state, prev_action, not_done_masks, deterministic=False
         )
         actions.append(action[0].item())
-        if action[0].item() == 0:  # This is stop action
-            if visualize:
-                cv2.destroyAllWindows()
-                video.release()
+        prev_action = action
+        if action[0].item() == 0 or displacement[0] < 0.2:  # This is stop action
+            print("STOP")
             return 1
-        ob = sim.step(action[0].item())
-        if visualize:
+        ##        elif action[0].item() == 0 and displacement[0] >= 0.2:
+        ##            pu.db
+        ##            print("STOP")
+        ##            return -1
+        if video is not None:
             image = np.vstack([ob["rgb"], start_image, goal_image])
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+            cv2.putText(
+                image,
+                text=str(displacement).replace("tensor(", "").replace(")", ""),
+                fontFace=cv2.FONT_HERSHEY_SIMPLEX,
+                org=(10, 260),
+                fontScale=0.5,
+                color=(255, 255, 255),
+                thickness=2,
+            )
             video.write(image)
-            # visualize_observation(ob, start_image, goal_image)
-    if visualize:
-        cv2.destroyAllWindows()
-        video.release()
+        ob = sim.step(action[0].item())
     return 0
 
 
+def angle_between_vectors(v1, v2):
+    return np.arccos((v1 @ v2) / (np.linalg.norm(v1) * np.linalg.norm(v2)))
+
+
 def get_displacement_local_goal(sim, local_goal, d):
+    # See https://github.com/facebookresearch/habitat-lab/blob/b7a93bc493f7fb89e5bf30b40be204ff7b5570d7/habitat/tasks/nav/nav.py
+    # for more information
     pos_goal, rot_goal = get_node_pose(local_goal, d)
+    # Quaternion is returned as list, need to change datatype
+    rot_goal = quaternion(*rot_goal)
     pos_agent = sim.get_agent_state().position
-    diff = pos_goal - pos_agent
-    return np.array([diff[2], diff[0]])
+    rot_agent = sim.get_agent_state().rotation
+    direction_vector = pos_goal - pos_agent
+    direction_vector_agent = quaternion_rotate_vector(
+        rot_agent.inverse(), direction_vector
+    )
+    rho, phi = cartesian_to_polar(-direction_vector_agent[2], direction_vector_agent[0])
+
+    # Should be same as agent_world_angle
+    return np.array([rho, phi])
 
 
 def visualize_observation(observation, start, goal):
@@ -203,15 +243,20 @@ def visualize_observation(observation, start, goal):
 
 def run_experiment(G, d, model, hidden_state, scene, device, experiments=100):
     # Choose 2 random nodes in graph
+    # 0 means success
+    # 1 means failed at runtime
+    # 2 means no path found
     return_codes = [0 for i in range(3)]
-    for _ in range(experiments):
+    for _ in tqdm(range(experiments)):
         node1, node2 = get_two_nodes(G)
         # Hard code for now...
-        node1 = (13, 10)
-        node2 = (31, 60)
+        # node1 = (8, 10)
+        # node2 = (4, 60)
         results = try_to_reach(
             G, node1, node2, d, model, deepcopy(hidden_state), scene, device
         )
+        if results == 0:
+            print(node1, node2)
         return_codes[results] += 1
     return return_codes
 
@@ -239,8 +284,8 @@ def main():
     #    traj_new = np.load("./traj_new.npy", allow_pickle=True)
     #    traj_new_eval = np.load("./traj_new_eval.npy", allow_pickle=True)
     #    eval_trajs = np.load("./eval_trajs.npy", allow_pickle=True)
-    run_experiment(G, d, model, hidden_state, scene, device)
-    pu.db
+    results = run_experiment(G, d, model, hidden_state, scene, device)
+    print(results)
 
 
 if __name__ == "__main__":
