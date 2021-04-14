3019258bf059108b4fd434f76147a93352955576
Justin W <jbwasse2@illinois.edu>
2021-04-14 17:30:34 -0500

    ORBSLAM implemented and tested

--- src/top_map/test/test_pose.py
+++ src/top_map/test/test_pose.py
@@ -3,19 +3,69 @@ import rclpy
 import os
 import signal
 from multiprocessing import Process
-from testing_helper import play_rosbag
+from testing_helper import play_rosbag, run_node
+from rclpy.node import Node
+from geometry_msgs.msg import Pose
 
 
+class PoseTester(Node):
+    def __init__(self):
+        super().__init__("pose_tester")
+        self.subscription = self.create_subscription(
+            Pose, "pose", self.pose_callback, 1
+        )
+        self.results = False
+        self.timer = self.create_timer(30, self.timer_callback)
+
+    def pose_callback(self, msg):
+        self.results = True
+
+    def timer_callback(self):
+        self.destroy_node()
+
+
+# Running this test seems to break the other one.
+# TODO figure out why
+# def test_tp_to_pose():
+#    rclpy.init()
+#    tp = (100, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0)
+#    pose = Orbslam2Pose(visualize=False)
+#    position, rotation = pose.trajectory_point_to_pose(tp)
+#    assert position[0] == 0.0 and position[1] == 0.0 and position[2] == 0.0
+#    assert (
+#        rotation.w == 1.0
+#        and rotation.x == 0.0
+#        and rotation.y == 0.0
+#        and rotation.z == 0.0
+#    )
+#    rclpy.shutdown()
+
+
+# Check to make sure a pose message gets published
 def test_orbslam2():
     rclpy.init()
     rosbag_location = "./test/rosbag/rosbag2_2021_04_14-09_01_00"
+    pose_args = {"visualize": False}
+    p2 = Process(
+        target=run_node,
+        args=(
+            Orbslam2Pose,
+            pose_args,
+        ),
+    )
+    p2.start()
     p = Process(
         target=play_rosbag,
         args=(rosbag_location, False),
     )
-    pose = Orbslam2Pose(visualize=False)
     p.start()
-    rclpy.spin(pose)
+    pose = PoseTester()
+    rclpy.spin_once(pose)
+    try:
+        pose.destroy_node()
+    except Exception as e:
+        print(e)
     os.kill(p.pid, signal.SIGKILL)
+    os.kill(p2.pid, signal.SIGKILL)
     rclpy.shutdown()
-    assert pose.results == True
+    assert pose.results is True
--- src/top_map/top_map/pose.py
+++ src/top_map/top_map/pose.py
@@ -1,14 +1,18 @@
 # Methods for getting the pose of the robot
 import pudb
 import numpy as np
+import quaternion
 
-# I have no idea why, but in order for orbslam2 to run on my system I need to import cv2 first even if I don't use it.
+# I have no idea why, but in order for orbslam2 to run on my system
+# I need to import cv2 first even if I don't use it.
 import cv2  # noqa
 import orbslam2
 from rclpy.node import Node
-import pudb
+import pudb  # noqa
 from cv_bridge import CvBridge
 from sensor_msgs.msg import Image
+from geometry_msgs.msg import Pose
+from std_msgs.msg import Header
 
 
 # TODO implement this for RGB or RGBD
@@ -30,17 +34,37 @@ class Orbslam2Pose(Node):
         self.subscription = self.create_subscription(
             Image, "camera", self.update_internal_state, 1
         )
+        self.publisher = self.create_publisher(Pose, "pose", 10)
         self.bridge = CvBridge()
-        self.results = False
         self.get_logger().info("ORBSLAM2 succesfully started")
-        self.instances = 0
 
-    # Takes in image as RGB and returns mono image to be added to orbslam2
+    # Takes in image as RGB and returns mono gray image to be added to orbslam2
     def mono_from_observation(self, observation):
         return np.dot(observation[..., :3], [0.2989, 0.5870, 0.1140])
 
+    # Takes 13 pt trajectory_point from orbslam2 wrapper and converts them into pose
+    # Format is as follows where Rwc is SE3 rotation and twc is position offset
+    # Time
+    # Rwc.at<float>(0,0),
+    # Rwc.at<float>(0,1),
+    # Rwc.at<float>(0,2),
+    # twc.at<float>(0),
+    # Rwc.at<float>(1,0),
+    # Rwc.at<float>(1,1),
+    # Rwc.at<float>(1,2),
+    # twc.at<float>(1),
+    # Rwc.at<float>(2,0),
+    # Rwc.at<float>(2,1),
+    # Rwc.at<float>(2,2),
+    # twc.at<float>(2)
+    def trajectory_point_to_pose(self, trajectory_point):
+        trajectory_point = np.array(trajectory_point[1:]).reshape(3, 4)
+        position = trajectory_point[:, 3]
+        rot_se3 = trajectory_point[0:3, 0:3]
+        rot_quat = quaternion.from_rotation_matrix(rot_se3)
+        return position, rot_quat
+
     def update_internal_state(self, msg):
-        self.instances += 1
         assert msg.height == 480
         assert msg.width == 640
         assert msg.encoding == "bgr8"
@@ -53,12 +77,9 @@ class Orbslam2Pose(Node):
             return
         image = self.bridge.imgmsg_to_cv2(msg, "bgr8")
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        gray = self.mono_from_observation(image)
-        #        gray = cv2.resize(gray, (256, 256), interpolation=cv2.INTER_AREA)
         current_time_second = msg.header.stamp.sec - self.start_time.sec
         current_time_nanosecond = msg.header.stamp.nanosec - self.start_time.nanosec
         current_time = current_time_second + (current_time_nanosecond * 10 ** -9)
-        current_time = self.instances * (1 / 30)
         self.slam.process_image_mono(image, current_time)
         if not str(self.slam.get_tracking_state()) == "OK":
             self.get_logger().warning(
@@ -73,3 +94,18 @@ class Orbslam2Pose(Node):
             self.get_logger().info(
                 'ORBSLAM2 succesfully updated state "%s"' % str(msg.header.stamp)
             )
+            header = Header()
+            header.frame_id = msg.header.frame_id
+            msg = Pose()
+            header.stamp = self.get_clock().now().to_msg()
+            position, rotation = self.trajectory_point_to_pose(
+                self.slam.get_trajectory_points()[-1]
+            )
+            msg.position.x = position[0]
+            msg.position.y = position[1]
+            msg.position.z = position[2]
+            msg.orientation.x = rotation.x
+            msg.orientation.y = rotation.y
+            msg.orientation.z = rotation.z
+            msg.orientation.w = rotation.w
+            self.publisher.publish(msg)
